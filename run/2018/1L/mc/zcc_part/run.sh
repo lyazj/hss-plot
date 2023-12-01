3a8f8dfe9bbc30ca93aa16b322c50e6a62602bb1
lyazj <seeson@pku.edu.cn>
2023-12-01 09:38:21 +0100

    Add running scripts for zcc/zss.

--- run/2018/1L/mc/zcc_part/run.sh
+++ run/2018/1L/mc/zcc_part/run.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 # usage: hist-hss-score <categorization-yaml> <lower-bound> <upper-bound> <branch-prefix> <signal-branch-suffix> <signal-category> <dir-to-root-files> [ <more-dir> ... ]
-exec ../../../../../bin/hist-hss-score \
-    ../categories.yaml \
+exec stdbuf -oL ../../../../../bin/hist-hss-score \
+    categories.yaml \
     0.0 \
     1.0 \
-    ak15_ParTMDv2_ \
+    ak15_ParTMDV2_ \
     Hcc \
     wzcc \
-    /eos/user/l/legao/hss/samples/Tree/2018/1L/mc/pieces
+    /eos/user/l/legao/hss/samples/Tree/2018/1L/mc/pieces \
+    2>&1 | tee run.log
