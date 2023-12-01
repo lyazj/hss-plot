04fed14b930f2d5ae13184ac0181139d5fc8b375
lyazj <seeson@pku.edu.cn>
2023-12-01 11:14:12 +0100

    Set configuration name properly.

--- run/2018/1L/mc/zss_part/run.sh
+++ run/2018/1L/mc/zss_part/run.sh
@@ -2,7 +2,7 @@
 
 # usage: hist-hss-score <categorization-yaml> <lower-bound> <upper-bound> <branch-prefix> <signal-branch-suffix> <signal-category> <dir-to-root-files> [ <more-dir> ... ]
 exec stdbuf -oL ../../../../../bin/hist-hss-score \
-    categories.yaml \
+    2018_1L_mc.yaml \
     0.0 \
     1.0 \
     ak15_ParTMDV2_ \
